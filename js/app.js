68b24b4d2d1126be8771b931ed658c26ad803d96
ipmhu <robelinmejia19@gmail.com>
2026-05-15 22:42:23 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 const CONFIG = {
     SUPABASE_URL: 'https://zgorzqfbqxnxcfzyirai.supabase.co',
     SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpnb3J6cWZicXhueGNmenlpcmFpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4MDc4MjYsImV4cCI6MjA5NDM4MzgyNn0.muFPtmZ-WDJV9HYP6Y4EdPK9CwfmzZqa73kvVoKSBJg',
-    OPENROUTER_API_KEY: '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'
+    OPENROUTER_API_KEY: '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'
 };
 
 const App = {
